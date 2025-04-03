6c83dedd35d422fae2ede5f6b03e555fa67c8f1b
Vpirus <glydelvillarino23@gmail.com>
2025-04-03 23:19:36 +0800

    updated account-routing

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -5,5 +5,24 @@ import { LayoutComponent } from '.layout.component';
 import { LoginComponent } from 'login.component';
 import { RegisterComponent } from './register.component';
 import { VerifyEmailComponent } from './verify-mail.component';
-import { ForgotPassword } from './forgot-password.component';
+import { ForgotPasswordComponent } from './forgot-password.component';
 import { ResetPasswordComponent} from './reset-password.component';
+
+const routes: Routes = [
+    {
+        path: '', components: LayoutComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+            { path: 'verify-email', component: VerifyEmailComponent },
+            { path: 'forgot-password', component: ForgotPasswordComponent },
+            { path: 'reset-password', component: ResetPasswordComponent }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AccountRoutingModule { }
